ee9f41c2d24db28d33cf15ab31674dfdc20a747e
Stanley <stanley@stanographer.com>
2019-01-21 22:46:48 -0500

    Added production URLs to client side.

--- app.js
+++ app.js
@@ -44,25 +44,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-/* Global path */
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-
-// catch 404 and forward to error handler
+/* catch 404 and forward to error handler */
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
+/* error handler */
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  /* render the error page */
   res.status(err.status || 500);
   res.render('error');
 });
--- client/src/components/ShareDB/connection.js
+++ client/src/components/ShareDB/connection.js
@@ -3,7 +3,7 @@ import ReconnectingWebSocket from 'reconnecting-websocket';
 import otText from 'ot-text';
 
 const server = {
-  host: window.location.hostname,
+  host: process.env.REACT_APP_ENV === 'production' ? 'upword.ly/ws' : window.location.hostname,
   port: process.env.REACT_APP_ENV === 'production' ? '' : ':9090',
   protocol: process.env.REACT_APP_ENV === 'production' ? 'wss://' : 'ws://',
   getAddress: function() {
